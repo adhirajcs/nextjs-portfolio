ef71d41e5dc029b1bdd7f2f4f2aa7a2f994978b9
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-09-12 17:18:46 +0000

    Added cool mode button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,7 @@
 import { TextGenerateEffect } from "./ui/text-generate-effect";
 import { TypewriterEffectSmooth } from "./ui/typewriter-effect";
 import { GlareCard } from "./ui/glare-card";
+import { CoolMode } from "./magicui/cool-mode";
 
 const Hero = () => {
   const words1 = [
@@ -18,30 +19,32 @@ const Hero = () => {
   const words2 = `I'm passionate about web development, blockchain technology, and building cool projects that make a difference. I love experimenting with the latest technologies and crafting experiences that are intuitive and engaging. My skills include ReactJS, Python, and Django.`;
 
   return (
-    <div className="flex flex-col lg:flex-row items-start lg:items-center justify-evenly relative z-10 text-white px-8 lg:px-56 py-10 h-screen">
-      {/* Left Side Content */}
-      <div className="text-justify lg:text-left space-y-4 pb-8 lg:pb-0">
-        <TypewriterEffectSmooth words={words1} />
-        <TextGenerateEffect words={words2} className="w-2/3" />
-      </div>
+    <div className="relative z-10 text-white h-screen px-8 lg:px-56 py-10 lg:pt-36">
+      <div className="flex flex-col lg:flex-row items-start lg:items-center justify-evenly">
+        {/* Left Side Content */}
+        <div className="text-justify lg:text-left space-y-4 pb-8 lg:pb-0">
+          <TypewriterEffectSmooth words={words1} />
+          <TextGenerateEffect words={words2} className="w-2/3" />
+        </div>
 
-      {/* Right Side Profile Picture */}
-      <div className="mb-6 lg:mb-0 flex-shrink-0">
-        <GlareCard className="flex flex-col items-center justify-center">
-          <img
-            src="/assets/profile-picture.png"
-            alt="Profile Picture"
-            className="w-full h-full object-cover"
-          />
-        </GlareCard>
+        {/* Right Side Profile Picture */}
+        <div className="mb-6 lg:mb-0 flex-shrink-0">
+          <GlareCard className="flex flex-col items-center justify-center">
+            <img
+              src="/assets/profile-picture.png"
+              alt="Profile Picture"
+              className="w-full h-full object-cover"
+            />
+          </GlareCard>
+        </div>
       </div>
-      {/* <div className="mb-6 lg:mb-0 flex-shrink-0">
-        <img
-          src="/assets/profile-picture.png"
-          alt="Profile Picture"
-          className="w-32 h-32 lg:w-40 lg:h-40 object-cover rounded-full shadow-lg"
-        />
-      </div> */}
+      <div className="flex flex-col items-start lg:items-end lg:pt-4">
+        <CoolMode>
+          <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            Click Me!
+          </button>
+        </CoolMode>
+    </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,20 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+
+      animation: {
+        shimmer: "shimmer 2s linear infinite",
+      },
+      keyframes: {
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
+      },
     },
   },
   plugins: [addVariablesForColors],
