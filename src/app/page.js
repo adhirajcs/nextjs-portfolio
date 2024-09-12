2eb7863c8300c2dd26c70e0bffb84211cf61cc46
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-09-12 17:30:37 +0000

    Added Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Background from "@/components/Background";
 import Hero from "@/components/Hero";
 import { BackgroundBeams } from "@/components/ui/background-beams";
+import NavBar from "@/components/NavBar";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <Background />
         <Hero />
         <BackgroundBeams />
+        <NavBar />
       </div>
     </>
   );
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,84 @@
+"use client";
+
+import React from "react";
+import { FloatingDock } from "@/components/ui/floating-dock";
+import {
+  IconBrandGithub,
+  IconBrandX,
+  IconExchange,
+  IconHome,
+  IconNewSection,
+  IconTerminal2,
+} from "@tabler/icons-react";
+import Image from "next/image";
+
+const NavBar = () => {
+  const links = [
+    {
+      title: "Home",
+      icon: (
+        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+
+    {
+      title: "Products",
+      icon: (
+        <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Components",
+      icon: (
+        <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Aceternity UI",
+      icon: (
+        <Image
+          src="/assets/favicon.ico"
+          width={20}
+          height={20}
+          alt="Aceternity Logo"
+        />
+      ),
+      href: "#",
+    },
+    {
+      title: "Changelog",
+      icon: (
+        <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+
+    {
+      title: "Twitter",
+      icon: (
+        <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "GitHub",
+      icon: (
+        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+  ];
+  return (
+    <div className="absolute bottom-0 left-0 right-0 flex items-center justify-center h-[5rem] z-50">
+      <FloatingDock
+        ClassName="translate-y-20"
+        items={links}
+      />
+    </div>
+  );
+};
+
+export default NavBar;
