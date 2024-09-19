e121f07ea3e84b06e01e73ed078abb0f5eb7a093
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-09-19 16:14:23 +0000

    Fixed background's layout

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,10 +1,7 @@
 const Background = () => {
-    return (
-      <div className="fixed h-full w-full bg-slate-950">
-        
-      </div>
-    );
-  };
-  
-  export default Background;
-  
+  return (
+    <div className="fixed top-0 left-0 h-screen w-screen bg-slate-950 z-[-1]"></div>
+  );
+};
+
+export default Background;
