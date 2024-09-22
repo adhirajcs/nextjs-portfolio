aef870e8b2a2588012dd0ea0f2465fff0d82e54e
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-09-22 17:10:44 +0000

    Fix

--- src/components/ui/timeline.jsx
+++ src/components/ui/timeline.jsx
@@ -27,7 +27,7 @@ export const Timeline = ({
 
   return (
     (<div
-      className="w-full bg-white/50 dark:bg-slate-900/50 font-sans md:px-10 overflow-hidden md:overflow-visible"
+      className="w-full bg-white/50 dark:bg-slate-900/50 font-sans md:px-10 rounded-2xl overflow-hidden md:overflow-visible"
       ref={containerRef}>
       {/* <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
         <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
