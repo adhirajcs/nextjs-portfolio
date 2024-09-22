09d090bf6eb6e7352a94034c0a8a8059f96e06b1
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-09-22 08:41:40 +0000

    Adjusted the opacity of contact form

--- src/components/contact-form.jsx
+++ src/components/contact-form.jsx
@@ -12,7 +12,7 @@ export default function ContactForm() {
   };
 
   return (
-    <div className="max-w-md w-full mx-auto rounded-2xl p-4 md:px-8 shadow-input bg-white dark:bg-black">
+    <div className="max-w-md w-full mx-auto rounded-2xl p-4 md:px-8 shadow-input bg-white/80 dark:bg-slate-900/50">
       {/* <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
         Contact Me
       </h2> */}
@@ -39,7 +39,7 @@ export default function ContactForm() {
         </LabelInputContainer>
 
         <button
-          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium"
+          className="bg-gradient-to-br relative group/btn from-black dark:from-slate-800 dark:to-slate-800 to-neutral-600 block dark:bg-slate-800 w-full text-white rounded-md h-10 font-medium"
           type="submit"
         >
           Send Message &rarr;
--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -37,10 +37,10 @@ const Input = React.forwardRef(({ className, type, ...props }, ref) => {
       <input
         type={type}
         className={cn(
-          `flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent 
-           file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
-           focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
-           disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
+          `flex h-10 w-full border-none bg-gray-50 dark:bg-slate-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent 
+           file:text-sm file:font-medium placeholder:text-gray-400 dark:placeholder-text-gray-600 
+           focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-gray-400 dark:focus-visible:ring-gray-600
+           disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--gray-700)]
            group-hover/input:shadow-none transition duration-400`,
           className
         )}
@@ -85,10 +85,10 @@ const TextAreaInput = React.forwardRef(({ className, ...props }, ref) => {
     >
       <textarea
         className={cn(
-          `flex w-full h-32 border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm
-           file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
-           focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
-           disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
+          `flex w-full h-32 border-none bg-gray-50 dark:bg-slate-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm
+           file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-400 dark:placeholder-text-gray-600 
+           focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-gray-400 dark:focus-visible:ring-gray-600
+           disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--gray-700)]
            group-hover/input:shadow-none transition duration-400`,
           className
         )}
