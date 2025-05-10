5bcbb5e007cebcd044dfdc7477aa27b2acb5dcea
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2025-05-10 16:42:23 +0000

    Remove react scans script

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,8 +9,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* import this BEFORE any scripts */}
-        <script src="https://unpkg.com/react-scan/dist/auto.global.js"></script>        
         <link rel="icon" type="image/svg+xml" href="/assets/icon.png" />
       </head>
       <body>{children}</body>
